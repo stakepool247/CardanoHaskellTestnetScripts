e0fbc1f1d8041d3ed5259a06f0e00a68ecb1cf8a
stakepool247 <59128747+stakepool247@users.noreply.github.com>
2020-07-12 23:18:49 +0300

    update
    was pointing to old shelley config.

--- core.sh
+++ core.sh
@@ -3,5 +3,5 @@ cardano-node run \
   --database-path  ~/cardano-node/db/core/ \
   --socket-path ~/cardano-node/socket/core \
   --port 3000 \
-  --config ~/cardano-node/configuration/core/shelley_testnet-config.json \
+  --config ~/cardano-node/configuration/core/mainnet_candidate-config.json \
   --topology ~/cardano-node/configuration/core/mainnet_candidate-topology.json
