ec0bd3fc44b9c08bb0090531911d5fb5211c2247
stakepool247 <59128747+stakepool247@users.noreply.github.com>
2020-07-25 22:28:47 +0300

    update to MC3
    changing config files for MC3

--- relay1.sh
+++ relay1.sh
@@ -3,5 +3,5 @@ cardano-node run \
   --database-path ~/cardano-node/db/relay1/ \
   --socket-path ~/cardano-node/socket/relay1 \
   --port 3001 \
-  --config ~/cardano-node/configuration/relay1/mainnet_candidate-config.json \
-  --topology ~/cardano-node/configuration/relay1/mainnet_candidate-topology.json
+  --config ~/cardano-node/configuration/relay1/mainnet_candidate_3-config.json \
+  --topology ~/cardano-node/configuration/relay1/mainnet_candidate_3-topology.json
--- relay2.sh
+++ relay2.sh
@@ -3,5 +3,5 @@ cardano-node run \
   --database-path ~/cardano-node/db/relay2/ \
   --socket-path ~/cardano-node/socket/relay2 \
   --port 3002 \
-  --config ~/cardano-node/configuration/relay2/mainnet_candidate-config.json \
-  --topology ~/cardano-node/configuration/relay2/mainnet_candidate-topology.json
+  --config ~/cardano-node/configuration/relay2/mainnet_candidate_3-config.json \
+  --topology ~/cardano-node/configuration/relay2/mainnet_candidate_3-topology.json
