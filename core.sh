703c6859db7c7858fa7c6aa9ac78e70f9f4342eb
stakepool247 <59128747+stakepool247@users.noreply.github.com>
2020-07-26 16:39:01 +0300

    Update for MC4
    updated config files

--- core.sh
+++ core.sh
@@ -3,5 +3,5 @@ cardano-node run \
   --database-path  ~/cardano-node/db/core/ \
   --socket-path ~/cardano-node/socket/core \
   --port 3000 \
-  --config ~/cardano-node/configuration/core/mainnet_candidate_3-config.json \
-  --topology ~/cardano-node/configuration/core/mainnet_candidate_3-topology.json
+  --config ~/cardano-node/configuration/core/mainnet_candidate_4-config.json \
+  --topology ~/cardano-node/configuration/core/mainnet_candidate_4-topology.json
